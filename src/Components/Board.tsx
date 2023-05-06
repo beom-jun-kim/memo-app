9406762f431aac86b942d196672705525c8dd46d
kimbeomjun <gimb52043@gmail.com>
2023-05-06 17:21:59 +0900

    드래그 색상

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -1,20 +1,33 @@
 import { Droppable } from "react-beautiful-dnd";
 import DraggableCard from "./DraggableCard";
 import styled from "styled-components";
-
-const Wrapper = styled.ul`
-  padding: 30px 10px;
+const Wrapper = styled.div`
+  width: 300px;
   background: ${(prop) => prop.theme.boardColor};
   border-radius: 5px;
   min-width: 300px;
+  display: flex;
+  flex-direction: column;
+`;
+
+// styled component에게 isDraggingOver라는 prop을 받을 거라고 말해준다
+const Area = styled.div<IAreaProps>`
+  background-color: ${(prop) => (prop.isDraggingOver ? "#dfe6e9" : prop.isDraggingFromThis ? "#b2bec3" : "#74b9ff")};
+  flex-grow: 1;
+  transition: .3s;
+  padding: 15px;
 `;
 
 const Title = styled.div`
-  text-align:center;
+  text-align: center;
   font-weight: 600;
-  margin-bottom: 15px;
-  
+  padding: 15px;
 `;
+
+interface IAreaProps {
+  isDraggingOver: boolean;
+  isDraggingFromThis:boolean;
+}
 
 interface IBoardProps {
   memos: string[];
@@ -23,21 +36,28 @@ interface IBoardProps {
 
 function Board({ memos, boardId }: IBoardProps) {
   return (
-    <div>
+    <Wrapper>
+      <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(provided /* 인자이니 이름 맘대로 */) => (
+        {(provided, snapshot) => (
+          // isDraggingOver : 유저가 board 위로 드래그 해서 들어오고 있는지 알려줌
+          // draggingFromThisWidth: 유저가 해당 board로 부터 드래그를 시작했는지 알려줌
           // innerRef, droppableProps : Droppable가 제공하는 속성
           // Droppable 컴포넌트의 드롭 영역으로 설정하고 해당영역을 제어
-          <Wrapper ref={provided.innerRef} {...provided.droppableProps}>
-            <Title>{boardId}</Title>
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
             {memos.map((memo, index) => (
               <DraggableCard key={memo} memo={memo} index={index} />
             ))}
             {provided.placeholder}
-          </Wrapper>
+          </Area>
         )}
       </Droppable>
-    </div>
+    </Wrapper>
   );
 }
 
--- src/Components/DraggableCard.tsx
+++ src/Components/DraggableCard.tsx
@@ -1,26 +1,29 @@
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
-import React from 'react';
+import React from "react";
 
-const Card = styled.li`
+const Card = styled.div<{ isDragging: boolean }>`
   padding: 10px;
-  background: ${(prop) => prop.theme.cardcolor};
+  background: ${(props) =>
+    props.isDragging ? "green" : props.theme.cardcolor};
   border-radius: 5px;
   margin-bottom: 10px;
+  box-shadow: ${props => props.isDragging? "2px 5px 5px rgba(0, 0, 0, 0.128)" : "none"};
 `;
 
 interface IDraggableCardPros {
-  memo:string;
-  index:number;
+  memo: string;
+  index: number;
 }
 
-function DraggableCard({memo, index}:IDraggableCardPros) {
-  console.log("memo",memo);
+function DraggableCard({ memo, index }: IDraggableCardPros) {
+  console.log("memo", memo);
   return (
     // Draggable의 key와 draggableId와 같아야 함 ☆☆☆
     <Draggable key={memo} draggableId={memo} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <Card
+          isDragging={snapshot.isDragging}
           ref={provided.innerRef}
           {...provided.draggableProps} /* box의 코너에서만 잡고 이동 가능하다 ======*/
           {...provided.dragHandleProps} /* 어딜 잡든 이동 가능 */
