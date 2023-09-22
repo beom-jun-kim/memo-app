9bd992a8e4fbcefcd4d3b873a30e8575b7273b30
kimbeomjun <gimb52043@gamil.com>
2023-09-22 17:28:07 +0900

    bg color 변경

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "red",
+  bgColor: "#2BAE66",
   boardColor: "#FCF6F5",
   cardcolor:"white",
 };
